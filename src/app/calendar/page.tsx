15b26c258bb76b4c1f085c14fff6013db75179de
Crystal Bajgai <crystalbajgai@gmail.com>
2025-02-20 22:03:14 +0545

    Set default lang as enGB for calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -4,7 +4,7 @@ const CalendarPage: React.FC = () => {
     return (
         <div className='h-screen flex flex-col justify-center items-center p-4'>
             <iframe 
-                src="https://calendar.google.com/calendar/embed?src=thebmcinsights%40britishmodelcollege.edu.np&ctz=Asia%2FKathmandu&showPrint=0" 
+                src="https://calendar.google.com/calendar/embed?src=thebmcinsights%40britishmodelcollege.edu.np&ctz=Asia%2FKathmandu&showPrint=0&hl=en_GB" 
                 className='w-full h-full'
                 style={{ border: 0 }}
             ></iframe>
